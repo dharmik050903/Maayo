49dc3ef1c09fb43bcfabf3a93520201d6e73523e
Dharmik <Dharmiksuthar0509@gmai.com>
2025-09-15 17:18:39 +0530

    Main logo Changed

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -3,7 +3,8 @@ export default function Logo({ theme = 'light' }) {
   return (
     <div className="flex items-center gap-2 select-none">
       <div className="w-9 h-9 rounded-lg bg-brand-gradient grid place-items-center border-0 outline-none">
-        <span className="text-white font-extrabold text-lg leading-none">M</span>
+        {/* <span className="text-white font-extrabold text-lg leading-none">M</span> */}
+        <img src="../MaayoLogo.jpg" alt="Maayo Logo"></img>
       </div>
       <span className={`text-xl font-semibold ${textClass}`}>Maayo</span>
     </div>
